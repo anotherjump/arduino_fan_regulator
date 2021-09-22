c4d4b7800ef7d1ac88e31c89fcce1ae8892d6999
anotherjump <88896625+anotherjump@users.noreply.github.com>
2021-09-22 16:00:02 +0300

    Update 4-digit-display-tachometer.ino

--- 4-digit-display-tachometer.ino
+++ 4-digit-display-tachometer.ino
@@ -1,79 +1,110 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
-#include <TimerOne.h>
-#include <TM1637.h>
+#include "TimerOne.h"
+#include "PinChangeInterrupt.h"
 
-const byte CLK = 5;  // 4-digit display CLK pin 
-const byte DIO = 6;  // 4-digit display DIO pin
+#define PIN_TACH_0 A0 // PIN A0 PCINT8
+#define INT_TACH_0 8
+
+#define PIN_TACH_1 A1 // PIN A1 PCINT9
+#define INT_TACH_1 9
+
+#define PIN_TACH_2 A2 // PIN A2 PCINT10
+#define INT_TACH_2 10
+
+#define PIN_TACH_3 A3 // PIN A3 PCINT11
+#define INT_TACH_3 11
+
 const byte FAN_PIN = 10;  //PWM pin
-const byte INT_PIN = 3;  // interrupt pin
-
 const int MIN_DUTY = 0; // 100% speed
-const int MAX_DUTY = 80; // 20% speed
-const int INIT_SPEED = 20;  // initial speed is 20%
+const int MAX_DUTY = 80; // 20% min speed
+const int INIT_SPEED = 25;  // initial speed is 25%
 
-volatile unsigned int counter = 0;  // interrupts counter
-unsigned int counter_buf = 0;       // stores current counter value while calculating
-unsigned long curr_millis;         // current millis
-unsigned long prev_millis;         // previous millis
-const unsigned long norm_cycle_time = 30000;  // 60 (seconds) * 1000 (milliseconds) / 2 ticks per cycle
+volatile unsigned int counters[4] = {0, 0, 0, 0};  // interrupt counters
+unsigned int counter_buf[4] = {0, 0, 0, 0};       // stores current counter value while calculating
+unsigned long curr_micros;         // current micros
+unsigned long prev_micros;         // previous micros
+//unsigned long prev_micros_debug;
+const unsigned long norm_cycle_time = 30000000;  // 60 (seconds) * 1000000 (microseconds) / 2 ticks per cycle
 float cycle_time;
-float rpm = 0;
+float rpm[4] = {0, 0, 0, 0};
 float incoming_speed;
 
-TM1637 tm(CLK,DIO);
+void setup() {
+  Serial.begin(9600);
+  Timer1.initialize(40);  // 40 us = 25 kHz
+  Timer1.pwm(FAN_PIN, calc_duty(INIT_SPEED));
+  pinMode(PIN_TACH_0, INPUT_PULLUP);
+  pinMode(PIN_TACH_1, INPUT_PULLUP);
+  pinMode(PIN_TACH_2, INPUT_PULLUP);
+  pinMode(PIN_TACH_3, INPUT_PULLUP);
 
-void countup() {
-  counter++;
+  attachPinChangeInterrupt(INT_TACH_0, countup_0, FALLING);
+  attachPinChangeInterrupt(INT_TACH_1, countup_1, FALLING);
+  attachPinChangeInterrupt(INT_TACH_2, countup_2, FALLING);
+  attachPinChangeInterrupt(INT_TACH_3, countup_3, FALLING);
+  sei();
 }
 
-void adjust_duty(){
+void loop() {
+  if (Serial.available() > 0) {
+    adjust_duty();
+  }
+  measure();
+  delay(2000);
+}
+
+void countup_0() {
+  counters[0]++;
+  //Serial.println(micros()-prev_micros_debug); //temporary debug stuff
+  //prev_micros_debug=micros();
+}
+
+void countup_1() {
+  counters[1]++;
+  //Serial.println(micros()-prev_micros_debug);
+  //prev_micros_debug=micros();
+}
+
+void countup_2() {
+  counters[2]++;
+  //Serial.println(micros()-prev_micros_debug);
+  //prev_micros_debug=micros();
+}
+
+void countup_3() {
+  counters[3]++;
+  //Serial.println(micros()-prev_micros_debug);
+  //prev_micros_debug=micros();
+}
+
+void adjust_duty() {
   incoming_speed = Serial.parseFloat();
-  if (incoming_speed >= 100 - MAX_DUTY & incoming_speed <= 100 - MIN_DUTY){
+  if (incoming_speed >= 100 - MAX_DUTY & incoming_speed <= 100 - MIN_DUTY) {
     Timer1.setPwmDuty(FAN_PIN, calc_duty(incoming_speed));
     Serial.print("speed set: ");
     Serial.println(incoming_speed, DEC);
   }
 }
 
-int calc_duty(float p){
-  return ((100.0 - p )/ 100) * 1023;
+int calc_duty(float p) {
+  return ((100.0 - p ) / 100) * 1023;
 }
 
-void measure(){
-  curr_millis = millis();
-  counter_buf = counter;
-  counter = 0;
-  cycle_time = float(curr_millis - prev_millis);
-  if (cycle_time > 0){      // timer number (curr_millis) will overflow (go back to zero), after approximately 50 days.
-    rpm = (counter_buf/cycle_time)*norm_cycle_time;
+void measure() {
+  curr_micros = micros();
+  memcpy(counter_buf, counters, sizeof(counters));
+  memset(counters,0,sizeof(counters));
+  cycle_time = float(curr_micros - prev_micros);
+  if (cycle_time > 0) {     // timer number (curr_micros) will overflow (go back to zero), after approximately 50 days.
+    for(short i=0; i <4; i++){
+      rpm[i] = (counter_buf[i]/cycle_time) * norm_cycle_time;
+      }
+  }
+  prev_micros = curr_micros;
+  for(short j=0; j<4; j++){
+      Serial.print(counter_buf[j], DEC);
+      Serial.print(" -> ");
+      Serial.println(rpm[j], DEC);
     }
-  //tm.display(rpm);
-  prev_millis = curr_millis;
-  Serial.print("revs: ");
-  Serial.println(counter_buf, DEC);
-}
-
-
-void setup() {
-  Serial.begin(9600);
-  Timer1.initialize(40);  // 40 us = 25 kHz
-  Timer1.pwm(FAN_PIN, calc_duty(INIT_SPEED));
-  tm.init();
-  tm.set(3);
-  pinMode(INT_PIN, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(INT_PIN), countup, RISING);
-  sei();
-}
-
-void loop() {
-  if (Serial.available() > 0){
-      adjust_duty();
-    }
-  tm.display(int(rpm));
-  Serial.println(rpm);
-  measure();
-  delay(2000);
 }
